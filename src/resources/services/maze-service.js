9e432f522776e439212068c328ad5f55a6739228
Esger Jellema <esgerj@gmail.com>
2020-01-05 15:13:45 +0100

    There's a hole in the wall

--- src/resources/services/maze-service.js
+++ src/resources/services/maze-service.js
@@ -6,20 +6,46 @@ export class MazeService {
 
     constructor(screenService) {
         this._screenService = screenService;
-        this.wallSize = this._screenService.spriteSize;
+        this._minBrickSize = 50;
     }
 
     initWalls() {
+        this._holes = 1;
+        this._holeSize = 100;
         this.mazeWalls = [];
+        this.wallSize = this._screenService.spriteSize;
+        this._wallWidth = this._screenService.arena.width;
         this.addWall();
     }
 
+    randomBetween(min = 0, max) {
+        let rand = Math.floor(Math.random() * (max - min))
+        return rand + min;
+    }
+
     addWall() {
+        let bricks = [];
+        bricks.push(this.getBrick(0, this._wallWidth - this._holeSize, true));
+        let totalWidth = bricks[0].width + this._holeSize;
+        // for (let i = 0; i < this._holes; i++) {
+        //     // More than 1 hole
+        // }
+        bricks.push(this.getBrick(totalWidth, this._wallWidth - totalWidth));
         let wall = {
             animate: true,
-            position: - this.wallSize
+            position: - this.wallSize,
+            bricks: bricks
         }
         this.mazeWalls.push(wall);
+    }
+
+    getBrick(left, maxWidth, randomWidth = false) {
+        let brick = {
+            x: left,
+            width: randomWidth ?
+                this.randomBetween(this._minBrickSize, maxWidth) : maxWidth
+        }
+        return brick;
     }
 
     lower() {
